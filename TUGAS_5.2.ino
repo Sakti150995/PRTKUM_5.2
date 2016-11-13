d77d0f888a01ce28b2cd8d629f6e86bd24562612
SAKTI ABDULLAH (4211511015) <scrmha@gmail.com>
2016-11-13 07:36:37 -0700

    TUGAS 5.2
    TUGAS 5.2

---
+++ TUGAS_5.2.ino
@@ -0,0 +1,12 @@
+//laporan pratiku 5.2 sakti abdullah
+void setup() {
+Serial.begin(115200);
+}
+void loop() {
+Serial.write(0xF0);
+Serial.write(0x1F);
+Serial.write(0x2F);
+Serial.write(0xF3);
+Serial.write(0xF4);
+delay(2000);
+}
